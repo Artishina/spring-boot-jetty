d5a9fdd4de6f9d6e47844d92d0b452ff7f46c852
Borisova Polina <borisova.p@otr.ru>
2021-02-20 23:41:53 +0300

    Added @InitBinder

--- src/main/java/com/hellospringdemo/controller/StudentController.java
+++ src/main/java/com/hellospringdemo/controller/StudentController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import java.util.Map;
 import javax.validation.Valid;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 
 @Controller
 @RequestMapping("/student")
@@ -16,6 +19,14 @@ public class StudentController {
 
     @Value("#{countryOptions}")
     private Map<String, String> countryOptions;
+
+    @InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 
     @RequestMapping("/showForm")
     public String showForm(Model model) {
