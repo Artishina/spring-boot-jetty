4eae896fd29f43eaff679687a4c88b44258e2526
Borisova Polina <borisova.p@otr.ru>
2021-02-26 14:24:36 +0300

    Added DB Connect

--- src/main/java/com/hellospringdemo/controller/StudentController.java
+++ src/main/java/com/hellospringdemo/controller/StudentController.java
@@ -6,13 +6,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Map;
+import java.util.Properties;
+
 import javax.validation.Valid;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import javax.servlet.http.HttpServletRequest;
+
+import org.postgresql.Driver;
+import java.sql.*;
 
 @Controller
 @RequestMapping("/student")
@@ -22,12 +30,12 @@ public class StudentController {
     private Map<String, String> countryOptions;
 
     @InitBinder
-	public void initBinder(WebDataBinder dataBinder) {
-		
-		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
-		
-		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
-	}
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model model) {
@@ -36,19 +44,18 @@ public class StudentController {
 
         model.addAttribute("student", student);
 
-        // add the country options to the model 
-        model.addAttribute("theCountryOptions", countryOptions); 
+        // add the country options to the model
+        model.addAttribute("theCountryOptions", countryOptions);
 
         return "student-form";
     }
 
     @RequestMapping("/processForm")
-    public String processForm(HttpServletRequest request,
-                                @Valid @ModelAttribute("student") Student student,
-                                BindingResult bindintResult) {
-        System.out.println("student: " + student.getFirstName());    
-        
-        System.out.println("BindingResult: " + bindintResult + "\n\n\n\n");                            
+    public String processForm(HttpServletRequest request, @Valid @ModelAttribute("student") Student student,
+            BindingResult bindintResult) {
+        System.out.println("student: " + student.getFirstName());
+
+        System.out.println("BindingResult: " + bindintResult + "\n\n\n\n");
 
         if (bindintResult.hasErrors()) {
             request.setAttribute("theCountryOptions", countryOptions);
@@ -57,5 +64,30 @@ public class StudentController {
 
         return "student-confirmation";
     }
-    
+
+    @RequestMapping("/connect")
+    public String connect() {
+
+        System.out.println("Connect!!!!!!!!!!!!!!!");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/student";
+            Properties props = new Properties();
+            props.setProperty("user", "postgres");
+            props.setProperty("password", "bucha1167");
+            props.setProperty("ssl", "false");
+            Connection conn = DriverManager.getConnection(url, props);
+
+            System.out.println("Connection succesful " + conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "helloworld";
+
+        // String url =
+        // "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
+        // Connection conn = DriverManager.getConnection(url);
+    }
 }
