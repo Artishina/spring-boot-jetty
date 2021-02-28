22ffe74941d5b1b61d9949ca2140730c51030daa
Borisova Polina <borisova.p@otr.ru>
2021-02-28 21:28:14 +0300

    Added Bi-Directional support - delete contact through customer

---
+++ src/main/java/com/hellospringdemo/hibernate/DeleteCustomerDetailDemo.java
@@ -0,0 +1,63 @@
+package com.hellospringdemo.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import com.hellospringdemo.hibernate.entity.Contacts;
+import com.hellospringdemo.hibernate.entity.Customers;
+
+public class DeleteCustomerDetailDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+                                .configure("/com/hellospringdemo/hibernate/hibernate.cfg.xml")
+								.addAnnotatedClass(Contacts.class)
+								.addAnnotatedClass(Customers.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
+			session.beginTransaction();
+
+			// get the customer detail object
+			int theId = 2;
+			Customers customer = 
+					session.get(Customers.class, theId);
+			
+			// print the customer detail
+			System.out.println("customer: " + customer);
+						
+			// print  the associated contact
+			System.out.println("the associated contact: " + 
+								customer.getContact());
+
+                                // now let's delete the instructor detail
+			System.out.println("Deleting customer: " 
+            + customer);
+
+            session.delete(customer);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			// handle connection leak issue
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
